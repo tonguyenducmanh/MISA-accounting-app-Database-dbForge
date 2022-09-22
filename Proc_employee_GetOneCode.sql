74ac8b633a2f7dcce1adb748e41139787c6a5ba3
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-22 21:39:57 +0700

    feat: Thêm proc mới phục vụ cho việc check trùng mã nhân viên

---
+++ Proc_employee_GetOneCode.sql
@@ -0,0 +1,28 @@
+﻿USE `misa.web08.gpbl.tnmanh`;
+
+DROP PROCEDURE IF EXISTS Proc_employee_GetOneCode;
+
+DELIMITER $$
+
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE Proc_employee_GetOneCode (IN v_EmployeeCode char(36) -- Mã nhân viên để kiểm tra xem có trùng không
+)
+COMMENT '
+  -- Author: TNMANH
+  -- Created date: 15/09/2022
+  -- Description: Kiểm tra xem mã nhân viên đã trùng hay chưa
+  -- Modified by:
+  -- Code chạy thử Call Proc_employee_GetOneCode("NV001");
+  '
+BEGIN
+  SELECT
+    e.EmployeeID,
+    e.EmployeeCode,
+    e.FullName
+  FROM employee e
+  WHERE e.EmployeeCode = v_EmployeeCode;
+END
+$$
+
+DELIMITER ;
